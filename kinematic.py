ad1729a7199d8c7b4250d8fd47cbcd5e8638a933
FriarTuck <friartuck@crunchbang.(none)>
2013-12-19 20:05:25 -0700

    initial commit

---
+++ kinematic.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+def intro():
+	print "vf = vi + a * t"
+	print "d = vi * t + .5 * a * t^2"
+	print "vf^2 = vi^2 + 2 * a * d"
+	print "d = ((vi +vf) / 2) * t \n"
+intro()
+
+def velocityFinal():
+	print "vf = vi + a * t"
+	vi = int(raw_input("Value for initial velocity: "))
+	a = int(raw_input("Value for acceleration: "))
+	t = int(raw_input("Value for time: "))
+	
+	vf = vi + a * t
+	print "final velocity is: ",
+	print  vf
+	print ""
+#velocityFinal()
+
+def distance():
+	print "d = vi * t + .5 * a * t^2"
+	vi = int(raw_input("Value for initial velocity: "))
+	a = int(raw_input("Value for acceleration: "))
+	t = int(raw_input("Value for time: "))
+	
+	d = vi * t + .5 * a * t ** 2
+	print "Distance is: "
+	print d
+#distance()
+
+def distance2():
+	print "d = ((vi +vf) / 2) * t"
+	vi = int(raw_input("Value for initial velocity: "))
+	t = int(raw_input("Value for time: "))
+	vf = int(raw_input("Value for final velocity: "))
+	
+	d = ((vi + vf) / 2) * t
+	print "Distance = ",
+	print d
+distance2()
