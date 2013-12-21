5ef0a29ae4a291bde5617de6111f529a25cb5a40
tuckerrc <tucker.r.chapman@gmail.com>
2013-12-21 16:08:19 -0700

    Remove extra unneeded functions

--- kinematic.py
+++ kinematic.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-<<<<<<< HEAD
+
 import math
 
 def intro():
@@ -25,39 +25,6 @@ def intro():
 		function += 16
 	print vi ,' ', vf ,' ', a ,' ', d ,' ', t ,' ', function
 	x = [vi, vf, a, d, t, function]
-	print variables	
-=======
-def velocityFinal():
-	"""Prints final velocity when given vi, a, and t."""
-	print "vf = vi + a * t"
-	vi = int(raw_input("Value for initial velocity: "))
-	a = int(raw_input("Value for acceleration: "))
-	t = int(raw_input("Value for time: "))
-	
-	vf = vi + a * t
-	print "final velocity is: ",
-	print  vf
-	print ""
-#velocityFinal()
-
-def distance():
-	"""Calculates distance if given vi, t, and a."""
-	print "d = vi * t + .5 * a * t^2"
-	vi = int(raw_input("Value for initial velocity: "))
-	a = int(raw_input("Value for acceleration: "))
-	t = int(raw_input("Value for time: "))
-	
-	d = vi * t + .5 * a * t ** 2
-	print "Distance is: ",
-	print d
-#distance()
-
-def distance2():
-	""""Calculates distance when given vi, vf, and t.""""
-	print "d = ((vi +vf) / 2) * t"
-	vi = int(raw_input("Value for initial velocity: "))
-	t = int(raw_input("Value for time: "))
-	vf = int(raw_input("Value for final velocity: "))
->>>>>>> 880e02fe0c345599cefe958a35ad85b97979c622
+	print x	
 	
 intro()	
