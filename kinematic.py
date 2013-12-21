47f4d091c78d60a11ddf47da6d1fcd36b8f5a31f
tuckerrc <tucker.r.chapman@gmail.com>
2013-12-21 16:01:28 -0700

    intro and ask for variables

--- kinematic.py
+++ kinematic.py
@@ -1,11 +1,32 @@
 #! /usr/bin/env python
-def intro():
-	print "vf = vi + a * t"
-	print "d = vi * t + .5 * a * t^2"
-	print "vf^2 = vi^2 + 2 * a * d"
-	print "d = ((vi +vf) / 2) * t \n"
-intro()
 
+<<<<<<< HEAD
+import math
+
+def intro():
+	print "Welcome to Tucker's Kinematic equations calculator"
+	print "Please enter the values (? = unknown)"
+	vi = raw_input("Initial Velocity: ")
+	vf = raw_input("Final Velocity: ")
+	a = raw_input("Acceleration: ")
+	d = raw_input("Distance: ")
+	t = raw_input("Time: ")
+	function = 0
+	print type(function)
+	if vi == '?':
+		function += 1
+	if vf == '?':
+		function += 2
+	if a == "?":
+		function += 4	
+	if d == '?':
+		function += 8
+	if t == '?':
+		function += 16
+	print vi ,' ', vf ,' ', a ,' ', d ,' ', t ,' ', function
+	x = [vi, vf, a, d, t, function]
+	print variables	
+=======
 def velocityFinal():
 	"""Prints final velocity when given vi, a, and t."""
 	print "vf = vi + a * t"
@@ -37,8 +58,6 @@ def distance2():
 	vi = int(raw_input("Value for initial velocity: "))
 	t = int(raw_input("Value for time: "))
 	vf = int(raw_input("Value for final velocity: "))
+>>>>>>> 880e02fe0c345599cefe958a35ad85b97979c622
 	
-	d = ((vi + vf) / 2) * t
-	print "Distance = ",
-	print d
-distance2()
+intro()	
